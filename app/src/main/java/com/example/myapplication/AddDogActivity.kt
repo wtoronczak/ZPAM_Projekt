32ae4fb773737c796e694aaab7f52c5ef73e98b6
wtoronaczak <weronika1515@gmail.com>
2024-06-13 13:50:27 +0200

    Add dog option, view, activity

---
+++ app/src/main/java/com/example/myapplication/AddDogActivity.kt
@@ -0,0 +1,195 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.Spinner
+import android.widget.Toast
+import com.example.myapplication.firestore.DogBreed
+import com.example.myapplication.firestore.DogCondition
+import com.example.myapplication.firestore.DogFirestore
+import com.example.myapplication.firestore.DogFirestoreHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class AddDogActivity : BaseActivity() {
+    private val dogFirestoreHandler = DogFirestoreHandler()
+
+    private var breedSpinner: Spinner? = null
+    private var conditionSpinner : Spinner? = null
+    private var sexSpinner : Spinner? = null
+    private var isPregnantSpinner : Spinner? = null
+
+    private var nameEditText: EditText? = null
+    private var weightEditText: EditText? = null
+    private var ageEditText: EditText? = null
+
+    private var sportActivitySeekBar: SeekBar? = null
+
+    private var submitButton: Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_dog)
+
+        val intent = intent
+        val userEmail = intent.getStringExtra("userEmail")
+
+        setupView()
+        setupSpinners()
+
+        submitButton?.setOnClickListener{
+            val isAddDogSuccess = addDog(userEmail)
+
+            if(isAddDogSuccess){
+                goToMainActivity(userEmail)
+                finish()
+            }
+        }
+    }
+
+    fun setupView(){
+        breedSpinner = findViewById(R.id.breedSpinner)
+        conditionSpinner = findViewById(R.id.conditionSpinner)
+        sexSpinner = findViewById(R.id.sexSpinner)
+        isPregnantSpinner = findViewById(R.id.isPregnantSpinner)
+
+        nameEditText = findViewById(R.id.nameEditText)
+        weightEditText = findViewById(R.id.weightEditText)
+        ageEditText = findViewById(R.id.ageEditText)
+
+        sportActivitySeekBar = findViewById(R.id.sportActivitySeekBar)
+
+        submitButton = findViewById(R.id.submitButton)
+    }
+    fun setupSpinners(){
+
+        //Breed dog spinner
+        val breedOptions = arrayOf("Select Breed", "small", "medium", "big", "huge")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, breedOptions)
+        adapter.setDropDownViewResource(R.layout.spiner_view_design)
+
+        breedSpinner?.adapter = adapter
+
+        //Dog Condition Spinner
+        val conditionOptions = arrayOf("Select Condition", "skinny", "normal", "chunky")
+
+        val conditionAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, conditionOptions)
+        conditionAdapter.setDropDownViewResource(R.layout.spiner_view_design)
+        conditionSpinner?.adapter = conditionAdapter
+
+
+        //Dog Sex Spinner
+        val sexSpinnerOptions = arrayOf("Select Sex", "male", "female")
+
+        val sexSpinnerAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, sexSpinnerOptions)
+        sexSpinnerAdapter.setDropDownViewResource(R.layout.spiner_view_design)
+        sexSpinner?.adapter = sexSpinnerAdapter
+
+
+        //isPregnant Spinner
+        val isPregnantSpinnerOptions = arrayOf("Is Pregnant?", "yes", "no")
+
+        val isPregnantSpinnerAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, isPregnantSpinnerOptions)
+        isPregnantSpinnerAdapter.setDropDownViewResource(R.layout.spiner_view_design)
+        isPregnantSpinner?.adapter = isPregnantSpinnerAdapter
+    }
+    fun addDog(userEmail: String?): Boolean{
+        // get string values
+        val nameStr = nameEditText?.text.toString()
+        val ageStr = ageEditText?.text.toString()
+        val weightStr = weightEditText?.text.toString()
+
+        val breedStr = breedSpinner?.getSelectedItem().toString()
+        val conditionStr = conditionSpinner?.getSelectedItem().toString()
+        val sexStr = sexSpinner?.getSelectedItem().toString()
+        val isPregnantStr = isPregnantSpinner?.getSelectedItem().toString()
+
+        val sportActivity = sportActivitySeekBar?.progress ?: 0
+
+        //validate and convert
+
+        if(nameStr.isBlank()){
+            Toast.makeText(this,"Dog name can't be empty",Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        if (ageStr.isBlank()) {
+            Toast.makeText(this, "Age can't be empty", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        val age = ageStr.toInt()
+
+        if (weightStr.isBlank()) {
+            Toast.makeText(this, "Weight can't be empty", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        val weight = ageStr.toDouble()
+
+        if (breedStr.isBlank()) {
+            Toast.makeText(this, "Breed can't be empty", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        val breed = try {
+            DogBreed.valueOf(breedStr)
+        } catch (e: IllegalArgumentException) {
+            Toast.makeText(this, "Please enter a proper breed value", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        if (conditionStr.isBlank()) {
+            Toast.makeText(this, "Condition can't be empty", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        val condition = try {
+            DogCondition.valueOf(conditionStr)
+        } catch (e: IllegalArgumentException) {
+            Toast.makeText(this, "Please enter a proper condition value", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        if (sexStr.isBlank() || (sexStr != "male" && sexStr != "female")) {
+            Toast.makeText(this, "Enter proper sex value", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        val isMale = sexStr == "male"
+
+        if (isPregnantStr.isBlank() || (isPregnantStr != "yes" && isPregnantStr != "no")) {
+            Toast.makeText(this, "Enter proper is pregnant value", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        val isPregnant = isPregnantStr == "yes"
+
+        if(userEmail == null){
+            Toast.makeText(this, "Can't find userEmail", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+
+        //create dog object
+        val dog = DogFirestore(nameStr, age, weight, breed, condition, sportActivity + 1, isPregnant, isMale, "", userEmail)
+
+
+        //add dog to firestore
+        GlobalScope.launch(Dispatchers.Main) {
+            // Dodanie psa do bazy danych Firestore
+            dogFirestoreHandler.addDog(dog)
+        }
+
+        return true
+    }
+
+    fun goToMainActivity(userEmail: String?) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("userEmail", userEmail)
+        startActivity(intent)
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,28 +16,37 @@ import com.example.myapplication.firestore.DogCondition
 import com.example.myapplication.firestore.DogFirestore
 import com.example.myapplication.firestore.DogFirestoreHandler
 import com.example.myapplication.recycleView.DogAdapter
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.*
 
 /**
  * Główna aktywność aplikacji, wyświetlająca powitanie użytkownika.
  */
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : BaseActivity() {
     private val dogFirestoreHandler = DogFirestoreHandler()
     private var welcomeTextView: TextView? = null
+    private var addDogButton: Button? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val userEmailTextView = findViewById<TextView>(R.id.userEmail)
+        addDogButton = findViewById(R.id.addDogButton)
 
+        val userEmailTextView = findViewById<TextView>(R.id.userEmail)
 
         // Pobranie identyfikatora użytkownika przekazanego z poprzedniej aktywności
         val intent = intent
         val userEmail = intent.getStringExtra("userEmail")
         userEmailTextView.text = userEmail
+
+        // Ustawienie nasłuchiwania kliknięć przycisku logowania
+        addDogButton?.setOnClickListener{
+            goToAddDogActivity(userEmail)
+            finish()
+        }
+
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
@@ -76,48 +88,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
         */
-        //Delete
-        /*val dogId = "d615e168-d511-44af-8a59-7e3a629df0a3"
-        GlobalScope.launch(Dispatchers.Main) {
-            //Usuniecie psa z bazy danych Firestore
-            dogFirestoreHandler.deleteDog(dogId)
-        }*/
 
-        //GetById
-        /*
-        val dogId = "158feb82-eb0f-4484-a443-b2894e6ebbc6"
-        lifecycleScope.launch(Dispatchers.Main) {
-            try {
-                Log.d("GetById", "Before")
-                val dog = dogFirestoreHandler.getByDogId(dogId)
-                if (dog != null) {
-                    Log.d("GetById", "Dog name ${dog.name}")
-                } else {
-                    Log.d("GetById", "Dog not found")
-                }
-            } catch (e: Exception) {
-                Log.e("GetById", "Error fetching dog by ID", e)
-            }
-        }
-         */
+    }
 
 
-
-        //GetAll
-        /*
-
-
-        if(userEmail != null) {
-            GlobalScope.launch(Dispatchers.Main) {
-                // Wszystkie psy użytkownika
-                Log.d("GetAll", "Before")
-                var dogs = dogFirestoreHandler.getAllUserDogs(userEmail)
-                Log.d("GetAll", "After + ${dogs.size}")
-                for(dog in dogs){
-                    Log.d("GetAll", "Dog name ${dog.name}")
-                }
-            }
-        }
-         */
+    fun goToAddDogActivity(userEmail: String?) {
+        val intent = Intent(this, AddDogActivity::class.java)
+        intent.putExtra("userEmail", userEmail)
+        startActivity(intent)
     }
 }
